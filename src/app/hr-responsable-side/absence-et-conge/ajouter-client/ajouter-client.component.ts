93780d13697c0a95ce67926f5752020722a1e896
hamzashadeau <60945657+hamzashadeau@users.noreply.github.com>
2020-09-13 13:42:23 +0100

    image info client

--- src/app/hr-responsable-side/absence-et-conge/ajouter-client/ajouter-client.component.ts
+++ src/app/hr-responsable-side/absence-et-conge/ajouter-client/ajouter-client.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { client } from 'src/app/controller/model/client.model';
 import { clientService } from 'src/app/controller/service/client.service';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-ajouter-client',
@@ -8,11 +9,26 @@ import { clientService } from 'src/app/controller/service/client.service';
   styleUrls: ['./ajouter-client.component.css']
 })
 export class AjouterClientComponent implements OnInit {
-
-  constructor(private clientService: clientService) { }
+  selectedFile: File;
+  uploadForm: FormGroup;
+  constructor(private clientService: clientService,
+              private formBuilder: FormBuilder) { }
 get clientAjout(): client {
   return this.clientService.clientAjout;
 }
+  public onFileChanged(event) {
+    this.selectedFile = event.target.files[0];
+    document.getElementById('lebel1').innerText = this.selectedFile.name;
+    this.clientAjout.imageName =  this.selectedFile.name.split('.').shift();
+    this.uploadForm.get('profile').setValue(this.selectedFile);
+  }
+  uploadFile() {
+    this.uploadForm.get('profile').setValue(this.selectedFile);
+    console.log(this.selectedFile);
+    const formData = new FormData();
+    formData.append('file', this.uploadForm.get('profile').value);
+    this.clientService.uploadFile(formData);
+  }
 categorie: string;
   public variables = ['Forces armées ', 'Police', 'finances', 'emploi', 'économie', 'justice', 'Laboratoire et police scientifique', 'Criminologie', 'Pompier', 'protection civile', 'ambulancier', 'Assureur', 'Banques ', 'Comptable', 'Expert comptable', 'Réviseur d’entreprises', 'Marketing', 'Délégué commercial', 'Secrétaire', 'Ingénieur de gestion', 'Gestion d’entreprise', 'Gestion des ressources humaines', 'autre'];
   public filteredList1 = this.variables.slice();
@@ -20,6 +36,9 @@ categorie: string;
   public filteredList2 = this.variables1.slice();
   ngOnInit(): void {
     this.categorie = 'client';
+    this.uploadForm = this.formBuilder.group({
+      profile: ['']
+    });
   }
   public showNumeroPermis(){
     if(this.clientAjout.possedePermis === true){
@@ -30,7 +49,9 @@ categorie: string;
     }
   }
 public ajouterUnclient(){
-  this.clientService.ajouterUnclient();
+    this.uploadFile();
+    this.clientService.save();
+  //this.clientService.ajouterUnclient();
 }
 public show(): boolean{
     if(this.clientAjout.profession === 'autre') {
